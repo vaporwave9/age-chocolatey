233f4d4a33f2f4f28f06125219f93b33f3d60491
vaporwave9 <64961250+vaporwave9@users.noreply.github.com>
2021-03-10 16:22:15 +0100

    Update chocolateyInstall.ps1

--- age.portable/tools/chocolateyInstall.ps1
+++ age.portable/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
     packageName    = $Env:ChocolateyPackageName
     unzipLocation  = Split-Path $MyInvocation.MyCommand.Definition
-    url64          = 'https://github.com/FiloSottile/age/releases/download/v1.0.0-beta7/age-v1.0.0-beta7-windows-amd64.zip'
-    checksum64     = '97f2f8fc6e7450ec60bbfb5207dfc18806af258f68915ca712bba0ffbb9e75a1' 
+    url64          = 'https://github.com/FiloSottile/age/releases/download/v1.0.0-rc.1/age-v1.0.0-rc.1-windows-amd64.zip'
+    checksum64     = 'A41A4C9D5F1B3F04C1C23F5E15B9129EA81250B88E0B7250081AB93F650AAF63' 
     checksumType64 = 'sha256'
 	SpecificFolder = 'age'
 }
